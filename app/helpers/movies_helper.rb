ad30fd4f4d64701393fc24c235f3341f737df371
nmhummel <natalie.hummel@gmail.com>
2021-01-22 22:42:14 -0600

    fixed flexboxes and made poster? helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,12 @@ module MoviesHelper
         end
     end
 
+    def poster?
+      if @movie.poster.attached?
+        image_tag url_for(@movie.poster.variant(resize: "150x200").processed.service_url)
+      else
+        image_tag("stinky-cheese.png", :size => '150x200')
+      end
+    end
+
 end
